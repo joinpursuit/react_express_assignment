9e697af5af2c4f3f48dcc4b38c2106ed6fdbf236
Muna Sharma <munasharma@pursuit.org>
2019-02-09 17:53:22 -0500

    edituser workingish

--- backend/db/usersQueries.js
+++ backend/db/usersQueries.js
@@ -1,17 +1,17 @@
-const pgp = require("pg-promise")({});
-const db = pgp("postgres://localhost:5432/userlist");
+const pgp = require('pg-promise')({});
+const db = pgp('postgres://localhost:5432/userlist');
 
 const getAllUsers = (req, res, next) => {
-  db.any("SELECT * FROM users")
+  db.any('SELECT * FROM users')
     .then(user => {
       res.status(200).json({
-        status: "success",
+        status: 'success',
         users: user,
-        message: "Got All Users"
+        message: 'Got All Users',
       });
     })
     .catch(err => {
-      console.log("error", err);
+      console.log('error', err);
       next(err);
     });
 };
@@ -19,54 +19,72 @@ const getAllUsers = (req, res, next) => {
 const getSingleUser = (req, res, next) => {
   const user_id = parseInt(req.params.id);
 
-  db.one("SELECT * FROM users WHERE id=$1", [user_id])
+  db.one('SELECT * FROM users WHERE id=$1', [user_id])
     .then(user => {
       res.status(200).json({
-        status: "success",
+        status: 'success',
         user: user,
-        message: "got single user"
+        message: 'got single user',
       });
     })
     .catch(err => {
-      console.log("error", err);
+      console.log('error', err);
       next(err);
     });
 };
 
 const createNewUser = (req, res, next) => {
   db.none(
-    "INSERT INTO users(name, phonenumber) VALUES( ${name}, ${phonenumber})",
+    'INSERT INTO users(name, phonenumber) VALUES( ${name}, ${phonenumber})',
     req.body
   )
     .then(() => {
       res.status(200).json({
-        message: "success"
+        message: 'success',
       });
     })
     .catch(err => {
-      console.log("error", err);
+      console.log('error', err);
       next(err);
     });
 };
 
 const editUser = (req, res, next) => {
+  // const userid = parseInt(req.params.id);
   db.none(
-    "UPDATE users SET name=${name} phonenumber=${phonenumber}  WHERE id=${id}",
+    'UPDATE users SET name= ${name}  WHERE id =${id}',
+    // [Number(req.params.id)],
     {
+      id: Number(req.params.id),
       name: req.body.name,
-      phonenumber: req.body.phonenumber,
-      id: parseInt(req.params.id)
     }
   )
     .then(() => {
       res.status(200).json({
-        status: "success",
-        message: "user has been updated"
+        status: 'success',
+        message: 'user has been updated',
       });
     })
     .catch(err => {
-      console.log("error", err);
+      console.log('error', err);
     });
 };
 
-module.exports = { getAllUsers, getSingleUser, createNewUser, editUser };
+const deleteUser = (req, res, next) => {
+  const user_id = parseInt(req.params.id);
+  db.none('DELETE FROM users WHERE id= $1', [user_id])
+    .then(() => {
+      res.status(200).json({ message: 'user deleted' });
+    })
+    .catch(err => {
+      console.log('error', err);
+    });
+};
+
+module.exports = {
+  getAllUsers,
+  getSingleUser,
+  createNewUser,
+  editUser,
+  deleteUser,
+};
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,15 +1,17 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 const {
   getAllUsers,
   getSingleUser,
   createNewUser,
-  editUser
-} = require("../db/usersQueries.js");
+  editUser,
+  deleteUser,
+} = require('../db/usersQueries.js');
 
-router.get("/", getAllUsers);
-router.get("/:id", getSingleUser);
-router.post("/", createNewUser);
-router.patch("/:id", editUser);
+router.get('/', getAllUsers);
+router.get('/:id', getSingleUser);
+router.post('/', createNewUser);
+router.patch('/:id', editUser);
+router.delete('/:id', deleteUser);
 
 module.exports = router;
