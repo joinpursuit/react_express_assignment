56ab3752badc54e35700cb403692637bbca68353
Andres.C <andrescabrera@pursuit.org>
2019-02-28 12:48:15 -0500

    db heroku

--- react-backend/db/queries/index.js
+++ react-backend/db/queries/index.js
@@ -1,4 +1,6 @@
 const pgp = require('pg-promise')({});
-const db = pgp('postgres://localhost:5432/reactexpress');
+const db = pgp(process.env.DATABASE_URL|| 'postgres://localhost:5432/reactexpress');
 
 module.exports = { db }
+
+// const db = pgp('postgres://localhost:5432/reactexpress');
